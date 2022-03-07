55371ef0d4de71a605ad6401beb7186be1a42df4
Kirill Saidov <kirillsaidovd@yahoo.com>
2022-03-07 12:53:06 +0600

    various optimizers added; ASGD improved accuracygit add -A!

--- python/main_cnnclassifier.py
+++ python/main_cnnclassifier.py
@@ -7,31 +7,49 @@ import matplotlib.pyplot as plt
 import cnnclassifier as cnn
 from yolov5model import getDevice
 
-# TRAINING 
+# TRAINING
+
+"""
+optim = optimizer function [
+    'Adam', 'RAdam', 'SGD',
+    'ASGD', 'Adagrad', 'Adadelta',
+    'AdamW', 'Adamax', 'RMSProp'
+]
+
+ASGD, SGD and Adadelta are the best optimizers.
+"""
 
 train_path = '../data/data_gender/train'
 val_path = '../data/data_gender/val'
-epochs = 12
+epochs = 17
 batch_size = 32
 lr = 0.05
+optimizer = 'ASGD'
 
 device = torch.device(getDevice())
 model = cnn.CNNClassifier(
-    num_classes = 2, 
+    num_classes = 2,
     batch_size = batch_size,
     img_width = 64,
     img_height = 64,
+    # age
     #img_norm_mean = [0.63154647, 0.48489257, 0.41346439],
     #img_norm_std = [0.21639832, 0.19404103, 0.18550038]
+    # gender
     img_norm_mean = [0.65625078, 0.48664141, 0.40608295],
     img_norm_std = [0.20471508, 0.17793475, 0.16603905]
 ).to(device)
 
 val_acc, loss = cnn.train(
-    model = model, 
-    train_path = train_path, 
-    val_path = val_path, 
+    model = model,
+    train_path = train_path,
+    val_path = val_path,
     epochs = epochs,
+    optim = optimizer,
+    momentum = 0.7,
+    alpha = 0.75,
+    lambd = 0.0001,
+    t0 = 1000000.0,
     learning_rate = lr
 )
 
@@ -64,12 +82,3 @@ for (key, value) in pred.items():
     print(f"{key}\t: {value}")
 
 """
-
-
-
-
-
-
-
-
-
